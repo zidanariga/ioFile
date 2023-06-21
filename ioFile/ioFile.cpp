6b60d48be43fc0cc81ae0709ea12f689346d8533
Apelicious <Apelicious@DESKTOP-7OGS3S6>
2023-06-21 14:52:16 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+	
+	//unlimitid loop untuk menulis
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhinta jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+
+	}
+}
